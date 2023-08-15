aba2b91883df8242afc39377ac5929ba1146ac85
andavian <ing.elbaum@gmail.com>
2023-08-15 00:34:00 -0300

    rutas creadas

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,12 +1,35 @@
-const { Router } = require('express');
+const { Router } = require("express");
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
-
 
 const router = Router();
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
+router.get("/pokemons", (req, res) => {
+  return res.status(200).send("esta ruta trae los pokemons");
+});
 
+router.get("/pokemons/:id", (req, res) => {
+  const { id } = parseInt(req.params);
+  return res.status(200).send("esta ruta trae el detalle de cada pokemon");
+});
+
+router.get("/pokemons", (req, res) => {
+  const { name } = req.query;
+  console.log("nombre", name);
+
+  if (name === "prueba")
+    return res.status(200).send("esta ruta trae los pokemons por nombre");
+});
+
+router.post("/pokemons", (req, res) => {
+  const pokemon = req.body;
+  return res.status(201).send("pokemon creado con exito");
+});
+
+router.get("/types", (req, res) => {
+  return res.status(200).send("esta ruta trae los tipos de pokemons");
+});
 
 module.exports = router;
