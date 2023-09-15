0da3ef3d1eba87050a1f3b64d94eec106d4ba8f0
andavian <ing.elbaum@gmail.com>
2023-09-14 21:24:36 -0300

    deploy

--- client/src/index.js
+++ client/src/index.js
@@ -8,7 +8,8 @@ import store from "./redux/store";
 import { Provider } from "react-redux";
 import axios from "axios";
 
-axios.defaults.baseURL = "http://localhost:3001/";
+//axios.defaults.baseURL = "http://localhost:3001/";
+axios.defaults.baseURL = "https://server-pokedex-pi-henry.onrender.com/";
 
 ReactDOM.render(
   <BrowserRouter>
